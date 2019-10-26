19e8ac920e63175cedf2e128343ece76aea6c3a9
Frazer26 <adrian1468@gmail.com>
2019-10-26 15:46:33 +0200

    Implement datepicker without month

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgbDatepickerNavigateEvent} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
-export class TopBarComponent implements OnInit {
+export class TopBarComponent {
 
   constructor() {
   }
 
-  ngOnInit() {
+  dateNavigate($event: NgbDatepickerNavigateEvent) {
+    console.log($event.next.month);
+    console.log($event.next.year);
   }
 
 }
